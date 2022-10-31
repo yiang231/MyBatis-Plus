dcea1817ab5928ce769314e4cbb1357d21aaa822
Yiang <1712064922@qq.com>
2022-10-31 21:02:47 +0800

    插入数据，主键生成策略

--- src/main/java/com/atguigu/mp/entity/User.java
+++ src/main/java/com/atguigu/mp/entity/User.java
@@ -1,11 +1,25 @@
 package com.atguigu.mp.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 public class User {
+	@TableId(type = IdType.ASSIGN_ID)//第一种 默认主键生成策略Long型，来自twitter的雪花算法，数据库中用bigint类型
+//	@TableId(type = IdType.ASSIGN_UUID)//第二种 UUID去掉连接线长度32 实体类用String 数据库中用varchar至少32长度
+//	@TableId(type = IdType.INPUT)//第三种 自定义主键
+//	@TableId(type = IdType.AUTO)//第四种 类型中的id属性必须是整型
 	private Long id;
 	private String name;
 	private Integer age;
 	private String email;
+
+	public static void main(String[] args) {
+		System.out.println(UUID.randomUUID().toString());
+		//50183492-25a9-42ec-9a78-45c6d2b4a966
+		System.out.println(UUID.randomUUID().toString().length());//36
+	}
 }
--- src/test/java/com/atguigu/mp/MpTest.java
+++ src/test/java/com/atguigu/mp/MpTest.java
@@ -19,4 +19,21 @@ public class MpTest {
 		List<User> list = userMapper.selectList(null);
 		list.forEach(System.out::println);
 	}
+
+	@Test
+	public void test2() {
+		User user = new User();
+		user.setName("张三");
+		user.setAge(30);
+		user.setEmail("123@qq.com");
+
+		//添加数据
+		int rows = userMapper.insert(user);
+		//id 1587062368762974210 长度19的Long型
+		System.out.println("rows = " + rows);
+
+		//主键自动回填
+		//user = User(id=1587063016820600833, name=张三, age=30, email=123@qq.com)
+		System.out.println("user = " + user);
+	}
 }
