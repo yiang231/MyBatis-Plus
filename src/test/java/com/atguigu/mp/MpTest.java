bc419a6ff75fc0fd5673f756ae9fbd2a6c9993f0
Yiang <1712064922@qq.com>
2022-10-31 21:25:43 +0800

    修改数据，动态SQL问题

--- src/test/java/com/atguigu/mp/MpTest.java
+++ src/test/java/com/atguigu/mp/MpTest.java
@@ -36,4 +36,35 @@ public class MpTest {
 		//user = User(id=1587063016820600833, name=张三, age=30, email=123@qq.com)
 		System.out.println("user = " + user);
 	}
+
+	@Test
+	public void test3() {
+//		userMapper.updateById(entity);//根据主键修改
+//		userMapper.update(user, queryWrapper);//根据条件进行修改
+		User user = userMapper.selectById(1);
+		System.out.println("user修改前 = " + user);//此时User被查询出来，所有字段不为空
+
+		user.setName("李四");
+		user.setAge(21);
+		int rows = userMapper.updateById(user);//这是动态SQL
+		System.out.println("rows = " + rows);
+		System.out.println("user修改后 = " + user);
+		/*所有字段都有值？取决于这个字段是否为空，有就出现
+		==>  Preparing: UPDATE user SET name=?, age=?, email=? WHERE id=?
+		==> Parameters: 李四(String), 21(Integer), test1@baomidou.com(String), 1(Long)
+		* */
+	}
+
+	@Test
+	public void test4() {
+		User user = new User();
+
+		user.setName("goushidan");
+		user.setId(1L);
+		/*
+		==>  Preparing: UPDATE user SET name=? WHERE id=?
+		==> Parameters: goushidan(String), 1(Long)
+		* */
+		System.out.println(userMapper.updateById(user));
+	}
 }
